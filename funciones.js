b9a077fb0fbff6fe7f1b09a2b6e570fe53f9bb14
martinevillaf <109883432+martinevillaf@users.noreply.github.com>
2023-02-07 12:27:07 -0300

    ultimando

--- funciones.js
+++ funciones.js
@@ -1,7 +1,14 @@
 
-function redireccion(){
-    location.href  = ("https://twitter.com/?lang=es") };;
+function tw1(){
+    window.open("https://twitter.com/?lang=es", '_blank');}
+
+    
+function mail1(){
+    window.open("https://workspace.google.com/intl/es-419/products/gmail/?utm_source=google&utm_medium=cpc&utm_campaign=latam-AR-all-es-dr-bkws-all-all-trial-e-dr-1605540-LUAC0011865&utm_content=text-ad-none-any-DEV_c-CRE_471087538708-ADGP_Hybrid%20%7C%20BKWS%20-%20EXA%20%7C%20Txt%20~%20Gmail-KWID_43700057701729742-kwd-329871267&utm_term=KW_gmail-ST_gmail&gclid=CjwKCAiAioifBhAXEiwApzCztti0ijxiF36xEdi7BctIewdpGK7e_eyiVqLEBepuZEeBJL_t4RayWRoC060QAvD_BwE&gclsrc=aw.ds", '_blank');};;
 
 
+    function face1(){
+        window.open("https://es-la.facebook.com/", '_blank');}
+    
 
 
